a8099b4130f94e6fd221b6c1ff431160f415edc1
Thomas Leonard <talex5@gmail.com>
2014-07-18 15:35:14 +0100

    Don't try to resolve SSH DNS source
    Often makes ssh connections take ages.

--- build.sh
+++ build.sh
@@ -68,3 +68,4 @@ mount -o bind /proc /mnt/proc
 mount -o bind /dev /mnt/dev
 chroot /mnt apt-get -y update
 chroot /mnt apt-get -y install openssh-server ocaml ocaml-native-compilers camlp4-extra opam build-essential lvm2 aspcud pkg-config m4 libssl-dev parted --no-install-recommends
+echo UseDNS no >> etc/ssh/sshd_config
