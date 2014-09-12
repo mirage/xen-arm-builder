bca6dd171969a3b891fa0d5fb03e5f073fbe1764
Magnus Skjegstad <magnus@skjegstad.com>
2014-09-12 16:20:58 +0100

    add apt-get upgrade to build.sh

--- build.sh
+++ build.sh
@@ -84,6 +84,7 @@ mount -o bind /proc /mnt/proc
 mount -o bind /dev /mnt/dev
 
 chroot /mnt apt-get -y update
+chroot /mnt apt-get -y upgrade
 chroot /mnt apt-get -y install openssh-server ocaml ocaml-native-compilers camlp4-extra opam build-essential lvm2 aspcud pkg-config m4 libssl-dev libffi-dev parted avahi-daemon libnss-mdns iw batctl --no-install-recommends
 chroot /mnt apt-get -y install uuid-dev libxml2-dev libdevmapper-dev libpciaccess-dev libnl-dev libxen-dev libgnutls-dev --no-install-recommends
 
