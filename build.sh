759b7f9c11fdf140c44e04ca7ae4c14945f40897
Magnus Skjegstad <magnus@skjegstad.com>
2015-01-15 12:30:50 +0000

    Removed opam init and clone of opam-repository in build.sh, as it caused problems with opam later

--- build.sh
+++ build.sh
@@ -112,12 +112,13 @@ chroot /mnt mkdir -p /usr/include/xen/arch-arm/hvm
 chroot /mnt touch /usr/include/xen/arch-arm/hvm/save.h
 sed -i '/modprobe xen-gntdev/a modprobe xen-gntalloc' /mnt/etc/init.d/xen
 
-# OPAM init
-OPAM_ROOT=/home/mirage/.opam
-OPAM_REPO=/home/mirage/git/opam-repository
-git clone https://github.com/ocaml/opam-repository.git /mnt/${OPAM_REPO}
-chroot /mnt chown -R mirage ${OPAM_REPO}
-chroot /mnt opam init ${OPAM_REPO} -y --root=${OPAM_ROOT}
+# OPAM init - disabled for now
+#OPAM_ROOT=/home/mirage/.opam
+#OPAM_REPO=/home/mirage/git/opam-repository
+#git clone https://github.com/ocaml/opam-repository.git /mnt/${OPAM_REPO}
+#chroot /mnt chown -R mirage ${OPAM_REPO}
+#chroot /mnt opam init ${OPAM_REPO} -y --root=${OPAM_ROOT}
+
 # chroot /mnt opam repo add mirage https://github.com/mirage/mirage-dev.git --root=${OPAM_ROOT}
 # chroot /mnt opam update --root=${OPAM_ROOT} # due to a bug in 1.1.1 (fixed in 1.2)
 chroot /mnt chown -R mirage /home/mirage
