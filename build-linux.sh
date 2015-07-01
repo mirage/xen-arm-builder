492b467978e744830c840e5ed6683dfb06260b2f
Luke Dunstan <LukeDunstan81@gmail.com>
2015-07-02 00:24:40 +0800

    build-linux.sh: run depmod

--- build-linux.sh
+++ build-linux.sh
@@ -1,7 +1,12 @@
-#!/bin/sh -ex
+#!/bin/sh -eux
+
+sudo apt-get -y install kmod
 
 rm -rf linux-arm-modules
 cd linux
+KERNELVER=`make kernelversion`
 make ARCH=arm zImage dtbs -j 4
 make ARCH=arm modules -j 4
 make ARCH=arm INSTALL_MOD_PATH="`pwd`/../linux-arm-modules" modules_install -j 4
+cd ..
+depmod -b "`pwd`/linux-arm-modules" -F linux/System.map ${KERNELVER}
