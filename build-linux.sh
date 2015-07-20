31b4a2da6624edb25b8ba264a00c7e06b4ab5965
Luke Dunstan <LukeDunstan81@gmail.com>
2015-07-20 20:11:17 +0800

    Fix make kernelversion verbosity

--- build-linux.sh
+++ build-linux.sh
@@ -7,6 +7,6 @@ cd linux
 make ARCH=arm zImage dtbs -j 4
 make ARCH=arm modules -j 4
 make ARCH=arm INSTALL_MOD_PATH="`pwd`/../linux-arm-modules" modules_install -j 4
-KERNELVER=`make kernelversion`
+KERNELVER=`make --no-print-directory kernelversion`
 cd ..
 depmod -b "`pwd`/linux-arm-modules" -F linux/System.map ${KERNELVER}
