c6cae88635bf796ed67593d6586f79e5f6c4c9f8
Richard Mortier <mort@cantab.net>
2016-11-03 13:56:43 +0000

    clone: travis debug

--- clone.sh
+++ clone.sh
@@ -11,6 +11,7 @@ if [ ! -r $ALPINETGZ ]; then
     S=dl-cdn.alpinelinux.org
     curl -LO http://$S/alpine/v${ALPINEV%.*}/releases/armhf/$ALPINETGZ
     curl -LO http://$S/alpine/v$${ALPINEV%.*}/releases/armhf/$ALPINETGZ.asc
+    cat $ALPINETGZ.asc
     gpg -v $ALPINETGZ.asc
 fi
 
